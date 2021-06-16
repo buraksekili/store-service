cea018cc15752e05349664c8673e03d8607f9073
s21buraksekili <buraksekili@sabanciuniv.edu>
2021-06-16 18:42:10 +0300

    Add REST endpoints for DB operations

--- src/productservice/rest/rest.go
+++ src/productservice/rest/rest.go
@@ -9,7 +9,7 @@ import (
 )
 
 // ServerREST serves a REST API for the product service.
-// It takes addr to indicate which address should the server listen,
+// It takes addr for which address should the server listen,
 // and database handler which will operate on the database.
 func ServerREST(addr string, dh db.DBHandler) error {
 	r := mux.NewRouter()
@@ -18,8 +18,18 @@ func ServerREST(addr string, dh db.DBHandler) error {
 
 	// obtain sub-router for product router
 	pr := r.PathPrefix("/products").Subrouter()
-
 	pr.Methods("GET").Path("").HandlerFunc(h.getProducts)
+	pr.Methods("GET").Path("/{product_id}").HandlerFunc(h.findProduct)
+	pr.Methods("GET").Path("/vendor/{vendor_id}").HandlerFunc(h.findProductsByVendor)
+	pr.Methods("POST").Path("").HandlerFunc(h.addProduct)
+
+	cr := r.PathPrefix("/comments").Subrouter()
+	cr.Methods("GET").Path("").HandlerFunc(h.getComments)
+	cr.Methods("POST").Path("/{product_id}").HandlerFunc(h.addComment)
+
+	vr := r.PathPrefix("/vendors").Subrouter()
+	vr.Methods("GET").Path("").HandlerFunc(h.getVendors)
+	vr.Methods("POST").Path("").HandlerFunc(h.addVendor)
 
 	log.Printf("Listening on %s\n", addr)
 	return http.ListenAndServe(addr, r)
