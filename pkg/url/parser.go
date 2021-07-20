903767fe1edaeb5c4e48eda063f0e6a374e4348f
Burak Sekili <buraksekili@gmail.com>
2021-07-20 12:55:53 +0300

    Add a helper function to parse url query parameters
    Signed-off-by: Burak Sekili <buraksekili@gmail.com>

---
+++ pkg/url/parser.go
@@ -0,0 +1,24 @@
+package url
+
+import (
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/buraksekili/store-service/users"
+	"github.com/pkg/errors"
+)
+
+// ParseIntQueryParams parses integer query parameters
+// in the given request's URL.
+func ParseIntQueryParams(key string, def int, r *http.Request) (int64, error) {
+	strval := r.URL.Query().Get(key)
+	if strings.TrimSpace(strval) == "" {
+		return int64(def), nil
+	}
+	val, err := strconv.ParseInt(strval, 10, 64)
+	if err != nil {
+		return 0, errors.Wrap(err, users.ErrInvalidRequestPath.Error())
+	}
+	return val, err
+}
